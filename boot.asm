c88c1fcf55c73eae55dbf70be3b1067c5b2136ee
ezrawhuultra <ezra2013lvr@outlook.com>
2026-09-24 21:21:19 +0100

    i uploaded the wrong bootloader.
    im very silly

--- boot.asm
+++ boot.asm
@@ -1,32 +1,55 @@
-; boot.asm
-[BITS 32]
-[EXTERN kernel_main]
+[ORG 0x7C00]
+[BITS 16]
 
-section .multiboot
-align 4
-mb_header_start:
-    dd 0x1BADB002              ; magic number
-    dd 0x00                    ; flags
-    dd -(0x1BADB002 + 0x00)    ; checksum
-mb_header_end:
-
-section .text
-global _start
-_start:
-    ; Set up stack
-    mov esp, stack_top
-    
-    ; Call kernel
-    call kernel_main
-    
-    ; Infinite loop if kernel returns
+start:
     cli
-.halt:
-    hlt
-    jmp .halt
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7C00
+    
+    ; Print loading message
+    mov si, loading_msg
+    call print_string
+    
+    ; Load FAT32 kernel from disk
+    call load_kernel
+    
+    ; Jump to kernel because kernel is good.
+    jmp 0x9000:0x0000
 
-section .bss
-align 16
-stack_bottom:
-    resb 16384  ; 16 KiB stack
-stack_top:
+load_kernel:
+    mov ah, 0x02        ; Read sectors
+    mov al, 16          ; Read 16 sectors (8KB)
+    mov ch, 0           ; Cylinder
+    mov cl, 2           ; Start at sector 2
+    mov dh, 0           ; Head
+    mov dl, 0x80        ; First HDD
+    mov bx, 0x9000      ; Load to segment 0x9000
+    mov es, bx
+    xor bx, bx          ; Offset 0
+    int 0x13
+    jc disk_error
+    ret
+
+print_string:
+    lodsb
+    cmp al, 0
+    je done
+    mov ah, 0x0E
+    int 0x10
+    jmp print_string
+done:
+    ret
+
+disk_error:
+    mov si, error_msg
+    call print_string
+    jmp $
+
+loading_msg db 'Loading Soup OS...', 0
+error_msg db 'Disk error. UH OH!', 0
+
+times 510-($-start) db 0
+dw 0xAA55
